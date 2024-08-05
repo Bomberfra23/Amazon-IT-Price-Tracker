0e9113e91cb1f9f5a188714858be3a37bf1d2cd3
Franco Pisani <94381673+Bomberfra23@users.noreply.github.com>
2024-08-05 14:48:45 +0200

    Update settings.py

--- config/settings.py
+++ config/settings.py
@@ -1,14 +1,14 @@
 # Telegram Alert settings
-TELEGRAM_BOT_TOKEN: str = "" # use @botfather on Telegram
-TELEGRAM_CHAT_ID: str = ""   # use @chatIDrobot on Telegram
+TELEGRAM_BOT_TOKEN: str = "example" # use @botfather on Telegram
+TELEGRAM_CHAT_ID: str = "123456"   # use @chatIDrobot on Telegram
 
 # Email Alert settings
-EMAIL_SMTP_SERVER: str = ""   # smtp.example.com
-EMAIL_PORT: int =            # port number
-EMAIL_USE_TLS: bool =        # True or False
-EMAIL_HOST_USER: str =  ""    # sender@example.com
-EMAIL_HOST_PASSWORD: str = ""  # password
-EMAIL_ADDRESSEE: str = ""     # addresee@example.com
+EMAIL_SMTP_SERVER: str = "smtp.example.com"  
+EMAIL_PORT: int = 000           
+EMAIL_USE_TLS: bool = True      
+EMAIL_HOST_USER: str =  "sender@example.com"    
+EMAIL_HOST_PASSWORD: str = "password"  
+EMAIL_ADDRESSEE: str = "addresee@example.com"    
 
 
 
