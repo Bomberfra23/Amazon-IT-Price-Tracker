27b9c775288645a8054324c8f72fd88e42e76bd8
Franco Pisani <94381673+Bomberfra23@users.noreply.github.com>
2024-08-26 22:08:04 +0200

    Add files via upload

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import concurrent.futures
 import datetime, time
-from scripts.functions import task, read_data, urls, price_targets, telegram_alerts_settings, email_alerts_settings
+from scripts.functions import task, read_data, urls, price_targets, telegram_alerts_settings, email_alerts_settings, get_delay_input, get_threads_input, check_telegram_alert, check_email_alert
 from scripts.GUI import logo
 
 # main functions contains the main menu and thread's pool
@@ -8,15 +8,21 @@ def main():
     print(logo)
     input("Press ENTER to starting configuration and reading data")
     read_data()
-    number_of_threads = int(input("Insert the number of threads:   "))
-    tasks_delay = int(input("Insert the number of seconds of delay:  "))
+    number_of_threads = get_threads_input()
+    tasks_delay = get_delay_input()
+    check_telegram_alert()
+    check_email_alert()
+    input("Press ENTER to starting monitoring products")
     timestamp = datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')
     print(f"[{timestamp}]  Action: Ready!")
 
     while True:
+        
         start_time = time.time()
         with concurrent.futures.ThreadPoolExecutor(max_workers=number_of_threads) as executor:
-            results = executor.map(task, urls, price_targets, telegram_alerts_settings, email_alerts_settings) # parallelize product tasks using lists/dicts object as args
+            results = executor.map(task, urls, price_targets, telegram_alerts_settings, email_alerts_settings)
+            # parallelize product tasks using lists/dicts object as args
+        timestamp = datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')
         print(f"[{timestamp}]  Info : Tasks completed in: {time.time()-start_time}")
         time.sleep(tasks_delay) # delay between tasks
 
--- scripts/functions.py
+++ scripts/functions.py
@@ -9,6 +9,8 @@ from random_user_agent.user_agent import UserAgent
 import smtplib 
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
+from urllib3.exceptions import HTTPError, MaxRetryError, TimeoutError
+
 
 # Client User-Agent random generator variables
 software_names = [SoftwareName.CHROME.value, SoftwareName.EDGE.value, SoftwareName.FIREFOX.value, SoftwareName.ANDROID.value]
@@ -27,113 +29,357 @@ last_prices = dict()
 
 http_client = urllib3.PoolManager() # https requests pool manager
 
-class AmazonScraper:      # main scraper Class
+# functions assigned to manage the start input values on the main screen
+
+# main scraper Class
+class AmazonScraper:
      
     def __init__ (self, url: str):
           self.url = url
     
-    # function assigned to making http requests using random User-Agent and http pool manager
-    def make_request(self: classmethod) -> bytes:    
-        timestamp: str = datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')
-        headers: dict = {'User-Agent': user_agent_rotator.get_random_user_agent()}
-        try:
-           response = http_client.request("GET", self.url, headers=headers)
-           if response.status == 200:
-              html_content: bytes = response.data
-              return html_content   # returns html content in bytes when server status is 200                   
-           elif response.status == 404:
-              print(f'[{timestamp}]  Error: Page not found')
-           else:
-              print(f'[{timestamp}]  Error: Unexpected error')
-        except urllib3.exceptions as error: # errors handling
-              print(f'[{timestamp}]  HTTP Client Error: {error}')
-        except Exception as error:
-              print(f'[{timestamp}]  Generic Error: {error}')
-        finally:
-            response.release_conn()
+    # Class method assigned to making http requests using http pool manager
+    def make_request(self: classmethod, method = "GET", headers = None, params = None, data=None, timeout = 5.0, retries = 2, parse_json=False) -> dict:  
 
-    # function assigned to scraping the html_content output of the previous function
-    def scrape_data(self: classmethod, response: str) -> Tuple[float|str, str]:  
-        timestamp: str = datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+        if headers is None:
+            headers = {}
+
         try:
-            html_tree = html.fromstring(response)
-            title = html_tree.cssselect('#productTitle')[0].text_content().strip() # finds title
-            price_path = html_tree.cssselect('#apex_offerDisplay_desktop')
-            if price_path != []:
-                m = re.search(r'\d{1,3}?\.?\d{1,3}\,\d{1,2}', price_path[0].text_content().strip(), re.IGNORECASE) # finds price
-                price = float(m.group(0).replace(".","").replace(",", ".").replace("€", "")) # converts and formats the finded price
+
+            encoded_data = None
+            if data:
+                if isinstance(data, dict):
+                    encoded_data = json.dumps(data).encode('utf-8')
+                    headers['Content-Type'] = 'application/json'
+
+                else:
+                    encoded_data = data
+            
+            response = http_client.request(
+
+                method = method,
+                url = self.url, 
+                fields = params, 
+                body = encoded_data, 
+                headers = headers,
+                timeout = timeout,
+                preload_content = False
+                
+            )
+
+            data = response.data
+
+            if parse_json:
+                data = json.loads(data.decode('utf-8'))
+                return {
+
+                'status_code' : response.status,
+                'data' : data,
+                'error' : None
+                }
+            
             else:
-                price = "Out of Stock / No Offer" 
-        except Exception as error: # errors handling
-            print(f"[{timestamp}]  Generic Scraping Data Error: {error}")
-        print(f"[{timestamp}]  Object: {title}  Price: {price}€")
-        return title, price
-    
-    # function assigned to send Telegram notification using settings.py configuration
-    def telegram_alert(self: classmethod, telegram_bot_token: str, telegram_chat_id: str, title: str, price: float|str, url: str) -> None: 
+                tree = html.fromstring(data)
+                return {
+
+                'status_code' : response.status,
+                'data' : tree,
+                'error' : None
+
+                }
+            
+        except MaxRetryError as e:
+            
+            return {
+
+            'status_code': None,
+            'data': None,
+            'error': f"Max retries exceeded -> {str(e)}"
+
+            }
+        
+        except TimeoutError as e:
+
+            return {
+
+            'status_code': None,
+            'data': None,
+            'error': f"Request timed out -> {str(e)}"
+
+            }
+        
+        except HTTPError as e:
+            
+            return {
+
+            'status_code': None,
+            'data': None,
+            'error': f"HTTP error occurred -> {str(e)}"
+
+            }
+        except json.JSONDecodeError as e:
+
+            return {
+
+            'status_code': None,
+            'data': None,
+            'error': f"JSON decode error -> {str(e)}"
+
+            }
+        except Exception as e:
+
+            return {
+
+            'status_code': None,
+            'data': None,
+            'error': f"An unexpected error occurred -> {str(e)}"
+
+            }
+        finally:
+
+            if 'response' in locals():
+                response.release_conn()
+
+    # Class method assigned to scraping the html_content output of the previous function
+    def scrape_amazon_product(self: classmethod, response: dict) -> Tuple[float|str, str]:  
+
         timestamp: str = datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')
-        point_to_API: str = f"https://api.telegram.org/bot{telegram_bot_token}/sendMessage?chat_id={telegram_chat_id}&text={title}\n\nStatus: Available ✅\nPrice: {price}€\n\nLink: {url}"
-        try:
-            AmazonScraper(point_to_API).make_request() # makes request to Telegram API Server
+
+        if not response['error']:
+
+            try:
+
+                html_tree = response['data']
+                title = html_tree.cssselect('#productTitle')[0].text_content().strip() # finds title
+                price_path = html_tree.cssselect('#apex_offerDisplay_desktop')
+
+                if price_path != []:
+
+                    m = re.search(r'\d{1,3}?\.?\d{1,3}\,\d{1,2}', price_path[0].text_content().strip(), re.IGNORECASE) # finds price
+                    price = float(m.group(0).replace(".","").replace(",", ".").replace("€", "")) # converts and formats the finded price
+
+                else:
+
+                    price = "Out of Stock / No Offer" 
+                
+                vendor_list = html_tree.cssselect('#merchantInfoFeature_feature_div > div.offer-display-feature-text > div > span')
+
+                if vendor_list != []:
+                    
+                    vendor = vendor_list[0].text_content().strip()
+                
+                else:
+
+                    vendor = "N/A"
+                
+                rating_list = html_tree.cssselect('#acrPopover > span.a-declarative > a > span')
+
+                if rating_list != []:
+
+                    rating = float(rating_list[0].text_content().strip().replace(",", "."))
+                    
+                else:
+
+                    rating = 0
+        
+                print(f"[{timestamp}]  Object: {title}  Price: {price}€  Vendor: {vendor}  Rating: {rating}")
+                return price, title, vendor, rating
+            
+            except Exception as error: # errors handling
+                print(f"[{timestamp}]  Generic Scraping Data Error: {error}")
+
+        else:
+
+            print(f"[{timestamp}] HTTP Error: {response['error']}")
+            return None, None, None, None
+    
+    # Class method assigned to send Telegram notification using settings.py configuration
+    def telegram_message(self: classmethod, telegram_bot_token: str, telegram_chat_id: str, text: str) -> None: 
+        timestamp: str = datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+        API_call: str = f"https://api.telegram.org/bot{telegram_bot_token}/sendMessage"
+
+        data = {
+
+            'chat_id' : telegram_chat_id,
+            'text' : text,
+            'parse_mode' : 'HTML'
+
+        }
+
+        result = AmazonScraper(API_call).make_request(method='POST', data=data, parse_json=True) # makes request to Telegram API Server
+
+        if result['error'] or not result['data']['ok'] or result['status_code'] != 200:
+
+            print(f"[{timestamp}]  Error: Unable to send Telegram Alert!")
+
+        else:
+
             print(f"[{timestamp}]  Action: Telegram Alert sended!")
-        except Exception as error: #errors handling 
-            print(f"[{timestamp}]  Generic Telegram Alert Error: {error}")
     
-    # function assigned to send Email notification using settings.py configuration
-    def email_alert(self: classmethod, smtp_server: str, smtp_port: int, use_tls: bool, from_email: str, password: str, to_email: str, title: str, price: float|str, url: str) -> None: 
-        timestamp: str = datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')
-        msg = MIMEMultipart()
-        msg['From'] = from_email
-        msg['To'] = to_email
-        msg['Subject'] = "Amazon IT Price Tracker Alert"
-        body = f"{title}\n\nStatus: Available ✅\nPrice: {price}€\n\nLink: {url}"
-        msg.attach(MIMEText(body, 'plain'))
+    # Class method assigned to send Email notification using settings.py configuration
+    def email_message(self: classmethod, smtp_server: str, smtp_port: int, use_tls: bool, from_email: str, password: str, to_email: str, text: str) -> None: 
+
+        html_text = f"""   
+  
+        <html>
+           <body>
+             <p>{text}</p>
+           </body>
+        </html>
+
+        """
+
         try:
+
+            timestamp: str = datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+            msg = MIMEMultipart()
+            msg['From'] = from_email
+            msg['To'] = to_email
+            msg['Subject'] = "Amazon IT Price Tracker Alert"
+            msg.attach(MIMEText(html_text, 'html'))
             server = smtplib.SMTP(smtp_server, smtp_port)
+
             if use_tls:
                 server.starttls()
             server.login(from_email, password)
             text = msg.as_string()
             server.sendmail(from_email, to_email, text)
             server.quit()
+
             print(f"[{timestamp}]  Action: Email Alert sended!")
+
+        except smtplib.SMTPAuthenticationError as e:
+            print(f"[{timestamp}]  SMTP Server Authentication Email Alert Error: {e}")
+
+        except smtplib.SMTPConnectError as e:
+            print(f"[{timestamp}]  SMTP Server Connection Email Alert Error: {e}")
+
+        except smtplib.SMTPRecipientsRefused as e:
+            print(f"[{timestamp}]  Addresee Refused Email Alert Error: {e}")
+
+        except smtplib.SMTPDataError as e:
+            print(f"[{timestamp}]  Data Email Alert Error: {e}")
+
+        except smtplib.SMTPServerDisconnected as e:
+            print(f"[{timestamp}]  SMTP Server Disconnected Email Alert Error: {e}")
+
+        except smtplib.SMTPException as e:
+            print(f"[{timestamp}]  Generic SMTP Server Email Alert Error: {e}")
+
         except Exception as error:
-            print(f"[{timestamp}]  Generic Email Alert Error: {error}")
+            print(f"[{timestamp}]  Generic Email Alert Error: {e}")
     
 # function assigned to reading JSON file and extracting Amazon products, prices and alert configurations. Lists and dicts are initalized with those datas.
 def read_data() -> None:
+
     timestamp = datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')
     print(f"[{timestamp}]  Action: Starting...")
+
     try:
+
         with open('config/ASIN.json') as file_input:
             data = json.load(file_input)
             file_input.close()
+
         for product in data['amazon_it_products']:
             ASIN = str(product['ASIN'])
             price_target = float(product['price_target'])
             telegram_alert_setting = bool(product['telegram_alert'])
             email_alert_setting = bool(product["email_alert"])
             url = f'https://amazon.it/dp/{ASIN}'
-            last_prices[url] = 1000000
+            last_prices[url] = 0.0
             urls.append(url)
             price_targets.append(price_target)
             telegram_alerts_settings.append(telegram_alert_setting)
             email_alerts_settings.append(email_alert_setting)
+
         print(f"[{timestamp}]  Action: Successfully loaded {len(urls)} Amazon IT Products!")
+
     except Exception as error:
         print(f"[{timestamp}]  Generic Error: {error}")
         sys.exit()
 
 # function assigned to combine all the previous functions
 def task(url: str, price_target: float|str, telegram_alert_setting: bool, email_alert_setting: bool) -> None:
-     response = AmazonScraper(url).make_request()
-     title, price = AmazonScraper(url).scrape_data(response)
-     if type(price) == float and price<=price_target and price<last_prices[url]:
+     
+     headers: dict = {'User-Agent' : user_agent_rotator.get_random_user_agent(), "Accept" : "text/html"}
+     response = AmazonScraper(url).make_request(headers=headers)
+     price, title, vendor, rating = AmazonScraper(url).scrape_amazon_product(response)
+
+     if type(price) == float and price<=price_target and (price<last_prices[url] or last_prices[url] == 0.0):
+          
+          text = f"<b>{title}</b>\n\nRating: {rating} {int(round(rating))*'⭐️'}\nVendor: {vendor}\nStatus: Available ✅\nPrice: <b>{price}€</b> <del>{last_prices[url]}€</del>\n\n🔗Link: {url}"
+
           if telegram_alert_setting:
-              AmazonScraper(url).telegram_alert(TELEGRAM_BOT_TOKEN, TELEGRAM_CHAT_ID, title, price, url)
+              
+              AmazonScraper(url).telegram_message(TELEGRAM_BOT_TOKEN, TELEGRAM_CHAT_ID, text)
+
           if email_alert_setting:
-              AmazonScraper(url).email_alert(EMAIL_SMTP_SERVER, EMAIL_PORT, EMAIL_USE_TLS, EMAIL_HOST_USER, EMAIL_HOST_PASSWORD, EMAIL_ADDRESSEE, title, price, url)
+              
+              AmazonScraper(url).email_message(EMAIL_SMTP_SERVER, EMAIL_PORT, EMAIL_USE_TLS, EMAIL_HOST_USER, EMAIL_HOST_PASSWORD, EMAIL_ADDRESSEE, text)
+
      last_prices[url] = price
+
+# functions assigned to get user's input in the main page
+
+def get_threads_input() -> int:
+
+    value = input("Insert the number of threads:   ")
+
+    if value.isnumeric() and int(value) > 0:
+        return int(value)
+    
+    else:
+        print("Invalid Input! try again.")
+        return get_threads_input()
+
+def get_delay_input() -> int:
+
+    value = input("Insert the number of seconds of delay:  ")
+
+    if value.isnumeric() and int(value) >= 0:
+        return int(value)
+    
+    else:
+        print("Invalid Input! try again.")
+        return get_delay_input()
+
+# functions assigned to check if alerts work properly 
+
+def check_telegram_alert() -> None:
+    timestamp: str = datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+    value = input("Do You Want To Check If Telegram Alert Works Properly? [Y/n]  ")
+
+    if value.isalpha() and value == 'Y':
+        print(f"[{timestamp}]  Action: Checking Telegram API...")
+        text = "Telegram Alert Service has been started ✅"
+        AmazonScraper(None).telegram_message(TELEGRAM_BOT_TOKEN, TELEGRAM_CHAT_ID, text)
+    
+    elif value.isalpha() and value == 'n':
+        print(f"[{timestamp}]  Action: Telegram API Check has been skipped")
+    
+    else:
+        print("Invalid Input! try again.")
+        return check_telegram_alert()
+
+def check_email_alert() -> None:
+    timestamp: str = datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+    value = input("Do You Want To Check If Email Alert Works Properly? [Y/n]  ")
+
+    if value.isalpha() and value == 'Y':
+        print(f"[{timestamp}]  Action: Checking Email Alert Service...")
+        text = "Email Alert Service has been started ✅"
+        AmazonScraper(None).email_message(EMAIL_SMTP_SERVER, EMAIL_PORT, EMAIL_USE_TLS, EMAIL_HOST_USER, EMAIL_HOST_PASSWORD, EMAIL_ADDRESSEE, text)
+    
+    elif value.isalpha() and value == 'n':
+        print(f"[{timestamp}]  Action: Email Alert Service Check has been skipped")
+    
+    else:
+        print("Invalid Input! try again.")
+        return check_email_alert()
+
+
+
+
 
 
 
